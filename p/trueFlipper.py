b190f002aa201d54c393314e84ffb6b61813ec1f
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2022-09-11 15:57:01 -0700

    Add files via upload

--- p/trueFlipper.py
+++ p/trueFlipper.py
@@ -6,11 +6,14 @@ inpArr = []
 isTrue = True
 inp = '1'
 lastInp = '1'
+lastTrue = '1'
 trueArr = []
 
 trueArr.append(inp)
 
 while inp != '':
+
+    lastInp = inp
     
     inp = input('--- Enter value: ')
 
@@ -21,28 +24,32 @@ while inp != '':
     print(lastInp)
     print('----')
 
-    if isTrue and (lastInp == inp):
+    if isTrue and (inp in trueArr):
         inpArr.append(inp)
-        lastInp = inp
+        
         print('true: ' + str(inp))
-
-        
-        
+  
     else:
         inpArr.append(inp)
-        lastInp = inp
+
         if inpArr[-3:] == [inp, inp, inp]:
             print('inp : ' + str(inp) + ' is now true')
             if inp not in trueArr:
                 trueArr.append(inp)
+
             isTrue = True
         else:
             print('inp: ' + str(inp) + ' is false')
             isTrue = False
             
+    print('-----')
+    print('isTrue: ' + str(isTrue)) 
+    print('inp: ' + str(inp))
+    print('lastInp: ' + str(lastInp))
+#    print('lastTrue: ' + str(lastTrue))
     print('inpArr:')
     print(inpArr)
-    print('inArr[3]:')
+    print('inArr[=3]:')
     print(inpArr[-3:])
     print('trueArr:')
     print(trueArr)
