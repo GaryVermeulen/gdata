1ee00051e0f0f1d3ff3222de6d38bf043dd3c741
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2022-10-17 08:00:09 -0700

    Add files via upload

--- NeuralNets/nn1.py
+++ NeuralNets/nn1.py
@@ -17,14 +17,14 @@ def make_prediction(input_vector, weights, bias):
     layer_2 = sigmoid(layer_1)
     return layer_2
 
-target = 0
+target = 0 # Second target, first is 1
 
+# First section vectors
 input_vector = [1.72, 1.23]
-
 weights_1 = [1.26, 0]
 weights_2 = [2.17, 0.32]
 
-# Simple Pyton method to calc dot product
+# Simple pure Pyton method to calc dot product
 first_indexes_mult = input_vector[0] * weights_1[0]
 second_indexes_mult = input_vector[1] * weights_1[1]
 
@@ -42,12 +42,14 @@ dot_product_2 = np.dot(input_vector, weights_2)
 print(f"The NumPy vec 1 & weight 2 dot product is: {dot_product_2}")
 print('-' * 10)
 
+#
 # New (second) vectors
 input_vector = np.array([1.66, 1.56])
 weights_1 = np.array([1.45, -0.66])
 
 bias = np.array([0.0])
 
+# In this case prediction is Layer 2
 prediction = make_prediction(input_vector, weights_1, bias)
 
 print(f"The prediction result is: {prediction}")
