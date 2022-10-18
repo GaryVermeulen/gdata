be4249ccf2a2346a8ce9d672752533b7ced16798
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2022-10-18 10:17:39 -0700

    Add files via upload

---
+++ p/featureVector.py
@@ -0,0 +1,13 @@
+#
+from sklearn.feature_extraction.text import CountVectorizer
+
+sentences = ['John likes ice cream', 'John hates chocolate.']
+
+vectorizer = CountVectorizer(min_df=0, lowercase=False)
+vectorizer.fit(sentences)
+vv = vectorizer.vocabulary_
+print(vv)
+
+vt = vectorizer.transform(sentences).toarray()
+print(vt)
+
