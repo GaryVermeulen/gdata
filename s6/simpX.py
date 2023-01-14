35e35aa363a28b068a26b13af0ff507fcce06479
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2023-01-14 14:11:27 -0800

    Add files via upload

--- s6/simpX.py
+++ s6/simpX.py
@@ -19,28 +19,28 @@ class Thing:
 #
 class Vehicle(Thing):
 
-    _classInfo = 'Vehicle Class Root--Of Thing'
+    _classInfo = 'Vehicle Sub Class -- Of Thing'
 
     def __init__(self, name):
         self.name = name
 
 class Device(Thing):
 
-    _classInfo = 'Device Class Root--Of Thing'
+    _classInfo = 'Device Class Sub -- Of Thing'
 
     def __init__(self, name):
         self.name = name
 
 class Recreation_ground(Thing):
 
-    _classInfo = 'Recreation-ground Class Root--Of Thing'
+    _classInfo = 'Recreation-ground Sub Class -- Of Thing'
 
     def __init__(self, name):
         self.name = name
 
 class Animal(Thing):
 
-    _classInfo = 'Animal Class Root--Of Thing'
+    _classInfo = 'Animal Sub-Class -- Of Thing'
 
     def __init__(self, name):
         self.name = name
@@ -54,6 +54,8 @@ class Animal(Thing):
 # add knowledge (properties) to classes
 # build objects
 
+
+
 # globals
 #
 classes2Build = []
@@ -61,6 +63,60 @@ myVars = globals()
 buildStack = []
 nnList = []
 nnpList = []
+
+def constructor(self, arg):
+    self.constructor_arg = arg
+
+
+def makeObjects(sA):
+
+    names = []
+    objects = {}
+
+    print('--- makeObjects ---')
+
+    # Make all objects or just sentence objects?
+    # Hmmm, let's just make sentence objects for now...
+    #
+
+    print('    sA.inSent: ', sA.inSent)
+    print('    building sentence subject object(s)...')
+
+    subjectLst = sA.sSubj.split(';')
+
+    print('    subjectLst: ', subjectLst)
+
+    for s in subjectLst:
+        sLst = s.split(',')
+        names.append(sLst[0])
+
+    print('    names: ', names)
+#    print('    nnpList: ', nnpList)
+
+    for n in nnpList:
+
+        print('    n: ', n)
+        
+        if n[0] in names:
+            print('    n[0]: ', n[0])
+            print('    n[2]: ', n[2])
+            parentClass = n[2].capitalize()
+
+            print('    parentClass: ', parentClass)
+            
+            #myVars.__setitem__(n[0], type(n[0], (eval(parentClass), ), dict(name=n[0])))
+        
+            globals()[n[0]] = type(n[0], (eval(parentClass), ), dict(name=n[0]))
+
+            #objects[n[0]] = type(n[0], (eval(parentClass), ), {})
+            #print(objects)
+
+
+#    Daffy = Duck('Daffy')                               
+    
+
+    print('--- return makeObjects ---')
+    return
 
 
 def makeClass(c):
@@ -128,10 +184,52 @@ def extractNN(inData):
 
 if __name__ == "__main__":
 
+    ''' Sample test data when running as standalone
+
+    Input sentence w/corrected case:
+    ['Mary', 'walked', 'Pookie', 'in', 'the', 'park']
+    ----------------------
+    sPOS =   
+    sType =  declarative
+    sSubj =  Mary,NNP;Pookie,NNP
+    sVerb =  walked
+    sObj =   park,NN
+    sDet =   the
+    sIN =    in
+    sPP =    in,the,park,NN
+    sMD =
+    '''
+    class Sentence:
+
+        def __init__(self, inSent, sPOS, sType, sSubj, sVerb, sObj, sDet, sIN, sPP, sMD):
+            self.inSent = inSent
+            self.sPOS = sPOS
+            self.sType = sType
+            self.sSubj = sSubj
+            self.sVerb = sVerb
+            self.sObj = sObj
+            self.sDet = sDet
+            self.sIN = sIN
+            self.sPP = sPP
+            self.sMD = sMD
+
+    s = ['Mary', 'walked', 'Pookie', 'in', 'the', 'park']
+    sPOS = ''
+    sType = 'declarative'
+    sSubj = 'Mary,NNP;Pookie,NNP'
+    sVerb = 'walked'
+    sObj = 'park,NN'
+    sDet = 'the'
+    sIN = 'in'
+    sPP = 'in,the,park,NN'
+    sMD = ''
+
     print('=== simpX (main) ===')
 
     inData = getData()
     nnList, nnpList = extractNN(inData)
+
+    sA = Sentence(s, sPOS, sType, sSubj, sVerb, sObj, sDet, sIN, sPP, sMD)
     
     print('inData len: ', len(inData))
     print('inData type: ', type(inData))
@@ -142,3 +240,6 @@ if __name__ == "__main__":
     
     buildClasses()  # Define classes from 
 
+    makeObjects(sA)   # Construct objects (instances)
+
+    Daffy = Duck('Daffy')
