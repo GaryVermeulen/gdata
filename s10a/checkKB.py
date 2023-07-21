16f30901f373688f3c8fe1384e626363ddb0ac31
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2023-07-21 13:46:56 -0700

    Add files via upload

--- s10a/checkKB.py
+++ s10a/checkKB.py
@@ -70,6 +70,53 @@ if __name__ == "__main__":
     else:
         print('Adding: ', newNode, ppt, tag, canDo, newNodeParent)
         kbTree.add(newNode, ppt, tag, canDo, newNodeParent)
+        
+
+    # Add many children for rodentx test
+    #
+    newNode = 'rat'
+    ppt = 't'
+    tag = 'NN'
+    canDO = 'see,eat,walk,run'
+    newNodeParent = 'rodent' # which is a rodent which is a mammal
+
+    # Does parnet class exist?
+    node = kbTree.find_node(kbTree.root, newNodeParent)
+    if node == None:
+        print('Parent node not found.')
+    else:
+        print('Adding: ', newNode, ppt, tag, canDo, newNodeParent)
+        kbTree.add(newNode, ppt, tag, canDo, newNodeParent)
+
+
+    newNode = 'mouse'
+    ppt = 't'
+    tag = 'NN'
+    canDO = 'see,eat,walk,run'
+    newNodeParent = 'rodent' # which is a rodent which is a mammal
+
+    # Does parnet class exist?
+    node = kbTree.find_node(kbTree.root, newNodeParent)
+    if node == None:
+        print('Parent node not found.')
+    else:
+        print('Adding: ', newNode, ppt, tag, canDo, newNodeParent)
+        kbTree.add(newNode, ppt, tag, canDo, newNodeParent)
+
+
+    newNode = 'squirrel'
+    ppt = 't'
+    tag = 'NN'
+    canDO = 'see,eat,walk,run'
+    newNodeParent = 'rodent' # which is a rodent which is a mammal
+
+    # Does parnet class exist?
+    node = kbTree.find_node(kbTree.root, newNodeParent)
+    if node == None:
+        print('Parent node not found.')
+    else:
+        print('Adding: ', newNode, ppt, tag, canDo, newNodeParent)
+        kbTree.add(newNode, ppt, tag, canDo, newNodeParent)
 
     
     newNode = 'hamster'
@@ -110,21 +157,83 @@ if __name__ == "__main__":
     # Now let's try inserting a node between existing nodes...~?
     # Simple insert between parent and child
     # Ex: rodent - hamsterX - hamster
-    new_Node = 'hamsterX'
+    new_Node = 'rodentX'
     new_Parent = 'rodent'
     new_ppt = 'x'
     new_tag = 'XXX'
     new_canDo = 'x,y,z'
-    child_Node = 'hamster'
+    
+#    child_Node = 'hamster' # There may zero or many...
     
     # Does parnet class exist?
-    parent_node = kbTree.find_node(kbTree.root, new_Parent)
+    parentNode = kbTree.find_node(kbTree.root, new_Parent)
     
-    if parent_node == None:
-        print('Parent node not found.')
+    if parentNode == None:
+        print('Parent node not found: ', parentNode)
     else:
-        print('parent_node.key: ', parent_node.key)
-        print('Existing children is/are:', parent_node.children)
+        print('parentNode.key: ', parentNode.key)
+        print('Existing children is/are:', parentNode.children)
+
+        # Keep a copy of the parent children
+        parentNodeChildren = [] 
+        for c in parentNode.children:
+            parentNodeChildren.append(c)
+
+        print(len(parentNode.children))
+        print(len(parentNodeChildren))
+
+        print(parentNodeChildren)
+
+        print('-' * 5)
+
+        print('Adding/inserting new node: ', new_Node, new_ppt, new_tag, new_canDo, parentNode.key)
+        kbTree.add(new_Node, new_ppt, new_tag, new_canDo, parentNode.key)
+
+        insertedNode = kbTree.find_node(kbTree.root, new_Node)
+
+        print('insertedNode.key: ', insertedNode.key)
+        print('insertedNode.parentNode: ', insertedNode.parentNode)
+        print('children mess:')
+        print(insertedNode.children)
+        print(len(insertedNode.children))
+        print(type(insertedNode.children))
+            
+        for c in insertedNode.children:
+            print(c)
+                
+        print('-' * 5)
+
+        # Add the orignal parent children to inserted node
+        for c in parentNodeChildren:
+            insertedNode.children.append(c)
+
+
+        print(insertedNode.children)
+        print(len(insertedNode.children))
+        print(type(insertedNode.children))
+            
+        for c in insertedNode.children:
+            print(c)
+                
+        print('-' * 5)
+
+        print(parentNode.children)
+
+        # Keep the newly added child and clear the old children
+        keepChild = parentNode.children[-1]
+        parentNode.children.clear()
+        parentNode.children.append(keepChild)
+
+        print(parentNode.children)
+
+#        for c in parentNode.children:
+#            if c
+#            print(c)
+
+#        del parentNode.children[0] # I don't like just bindly removing [0]
+        
+
+        """
 
         if len(parent_node.children) == 1: # Assuming only "hamster"
             print('parent_node.children[0].key: ', parent_node.children[0].key)
@@ -163,7 +272,7 @@ if __name__ == "__main__":
         else:
             print('length greater then 1: ', len(parent_node.children))
 
-        
+        """
 
 
     
