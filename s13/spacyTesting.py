5862c7fba2902a91e03bd89b08deb12568ada41f
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2024-06-10 13:32:30 -0700

    Add files via upload

--- s13/spacyTesting.py
+++ s13/spacyTesting.py
@@ -1,14 +1,29 @@
 # Spacy testing
 #
 import spacy
+##import neuralcoref
 
 nlp = spacy.load('en_core_web_sm')
 
-doc = nlp('First, I wrote some sentences. Then spaCy parsed them. Hooray!')
+##neuralcoref.add_to_pipe(nlp,greedyness=0.52)
 
+doc = nlp('First, I wrote some sentences. Then spaCy parsed them. Hooray! Tom and Jerry ran in the park on Tuesday. They found Mark\'s baseball, and played with it.')
+
+##print(doc._.coref_resolved)
+print('----------------------')
 for token in doc:
     #print(type(token))
     #print(item)
     print('----')
     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
             token.shape_, token.is_alpha, token.is_stop)
+
+print('=====')
+
+#sent = next(doc.sents)
+#for word in sent:
+for sent in doc.sents:
+    for word in sent:
+        print(word, word.tag_, word.dep_)
+        print(spacy.explain(word.dep_))
+    
