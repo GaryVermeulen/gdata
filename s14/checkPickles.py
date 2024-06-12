f113fa3de798f4900631f0305c84f4cb3641ec2c
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2024-06-12 13:04:15 -0700

    Add files via upload

---
+++ s14/checkPickles.py
@@ -0,0 +1,100 @@
+#
+# pickleChecker.py
+#
+
+import pickle
+from commonConfig import Sentence
+
+
+def getPickles():
+
+    with open('data/ourDict.pkl', 'rb') as fp:
+        ourDict = pickle.load(fp)
+        print('Aunt Bee loaded ourDict.pkl')
+    fp.close()
+
+    with open('data/ourCorpus.pkl', 'rb') as fp:
+        ourCorpus = pickle.load(fp)
+        print('Aunt Bee loaded ourCorpus.pkl')
+    fp.close()
+
+    with open('data/newDict.pkl', 'rb') as fp:
+        newDict = pickle.load(fp)
+        print('Aunt Bee loaded newDict.pkl')
+    fp.close()
+    
+    return ourDict, ourCorpus, newDict
+
+
+
+if __name__ == "__main__":
+
+    """
+
+    ourDict, ourCorpus, newDict = getPickles()
+
+    print('ourDict:')
+    print(len(ourDict))
+    print(type(ourDict))
+    print(ourDict.get("pookie_1"))
+    print(ourDict.get("jimmy_1"))
+    
+    for key, value in ourDict.items():
+        print(key, value)
+        print(ourDict[key]['Word'])
+        print(ourDict[key]['Tag'])
+    print('-' * 5)
+    print('ourCorpus:')
+    print(len(ourCorpus))
+    print(type(ourCorpus))
+    print('-' * 5)
+    print('newDict:')
+    print(len(newDict))
+    print(type(newDict))
+    print(newDict.get("pookie_1"))
+    print(newDict.get("jimmy_1"))
+
+
+    objArr = pickle.load( open("data/objArr.p", "rb" ) )
+
+    print('len objArr: ', len(objArr))
+    print('type objArr: ', type(objArr))
+
+    for obj in objArr:
+        print('-----')
+        
+        #print('type obj: ', type(obj))
+        #print(obj)
+        obj.printAll()
+    
+    
+
+    untaggedBook = pickle.load(open('data/untaggedCorpora.p', 'rb'))
+
+    print('len untaggedBook: ', len(untaggedBook))
+    print('type untaggedBook: ', type(untaggedBook))
+
+    for sent in untaggedBook:
+        print('-----')
+        print(sent)
+        
+        
+    """
+
+    nlpDocs = pickle.load(open('data/nlpDocs.p', 'rb'))
+
+    print('len untaggedBook: ', len(nlpDocs))
+    print('type untaggedBook: ', type(nlpDocs))
+
+    docCnt = 0
+    for doc in nlpDocs:
+        docCnt += 1
+        
+        print('-----')
+        print(docCnt)
+        print(type(doc))
+        print(doc)
+
+        for word in doc:
+            print(word, word.tag_, word.dep_)
+        
